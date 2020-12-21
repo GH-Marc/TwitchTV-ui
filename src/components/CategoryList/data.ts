ba7488a7d8d91c7a36b8b5075c6f66df4e863a14
Marc <marcelopgm65@gmail.com>
2020-12-21 14:00:48 -0300

    :card_file_box: Category List data

---
+++ src/components/CategoryList/data.ts
@@ -0,0 +1,15 @@
+import lol from '../../images/category_lol.jpg';
+import valorant from '../../images/category_valorant.jpg';
+import osu from '../../images/category_osu.jpg';
+import overwatch from '../../images/category_overwatch.jpg';
+import vrchat from '../../images/category_vrchat.jpg';
+import aqw from '../../images/category_aqw.jpg';
+
+export default [
+  { name: 'League of Legends', source: lol },
+  { name: 'VALORANT', source: valorant },
+  { name: 'osu!', source: osu },
+  { name: 'Overwatch', source: overwatch },
+  { name: 'VRChat', source: vrchat },
+  { name: 'Adventure Quest Worlds', source: aqw },
+];
