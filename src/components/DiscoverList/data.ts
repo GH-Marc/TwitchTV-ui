9f19bf7800e263f1977b16ad75f59801e4076970
Marc <marcelopgm65@gmail.com>
2020-12-21 20:08:24 -0300

    :card_file_box: feat: Discover List data

---
+++ src/components/DiscoverList/data.ts
@@ -0,0 +1,13 @@
+import justchatting from '../../images/category_justchatting.jpg';
+import sot from '../../images/category_sot.jpg';
+import wow from '../../images/category_wow.jpg';
+import cyberpunk from '../../images/category_cyberpunk.jpg';
+import among from '../../images/category_among.jpg';
+
+export default [
+  { name: 'Só na conversa', source: justchatting, views: '319,2 mil espectadores', tag: 'Vida Real' },
+  { name: 'Sea of Thieves', source: sot, views: '9,9 mil espectadores', tag: 'Ação' },
+  { name: 'World of Warcraft', source: wow, views: '241,2 mil espectadores', tag: 'MMO' },
+  { name: 'Cyberpunk 2077', source: cyberpunk, views: '55 mil espectadores', tag: 'RPG' },
+  { name: 'Among Us', source: among, views: '74,8 mil espectadores', tag: 'Estratégia' },
+];
