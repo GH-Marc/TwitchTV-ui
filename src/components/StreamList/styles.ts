836adb9151ffe5afbeeac161a4fc830b44d89d9e
Marc <marcelopgm65@gmail.com>
2020-12-21 20:09:28 -0300

    :wrench:

--- src/components/StreamList/styles.ts
+++ src/components/StreamList/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 import colors from '../../styles/colors';
 
 export const List = styled.View`
-  padding: 8px 0 24px;
+  padding: 8px 0 15px;
 `;
 
 export const StreamContainer = styled.TouchableOpacity`
